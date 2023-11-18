90c853e6214860772646a66378624307103dfb1c
LZ <ge94zeg@mytum.de>
2023-11-18 11:17:56 +0100

    dummy db about log files

---
+++ server/logs/dummy.py
@@ -0,0 +1,31 @@
+log_db = {
+    "R&S": {
+        "19e303ce-32e3-493d-b710-d031ff162e90":
+            {
+                "logId": "19e303ce-32e3-493d-b710-d031ff162e90",
+                "fileName": f"final_log.out",
+                "description": "final log",
+                "uploadTime": 1700260617547,
+                "isProcessed": False,
+                "size": 21,
+            },
+        "78a99ab5-fe4d-4ede-b20b-86fa10c70d58":
+            {
+                "logId": "78a99ab5-fe4d-4ede-b20b-86fa10c70d58",
+                "fileName": f"test_log1.out",
+                "description": "log used for testing & development",
+                "uploadTime": 9428960614547,
+                "isProcessed": False,
+                "size": 4,
+            },
+        "32d86298-c3d1-4524-97f6-a1bc790ae6f8":
+            {
+                "logId": "32d86298-c3d1-4524-97f6-a1bc790ae6f8",
+                "fileName": f"test_log2.out",
+                "description": "log used for testing & development",
+                "uploadTime": 75289606763547,
+                "isProcessed": False,
+                "size": 6,
+            }
+    }
+}
