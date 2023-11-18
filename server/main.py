6711ec031219980a093142ca1dd3803332cab7ff
LZ <ge94zeg@mytum.de>
2023-11-18 09:55:57 +0100

    chat endpoint. for the moment returns dummy data

--- server/main.py
+++ server/main.py
@@ -3,7 +3,7 @@ from summarization import routes as summarize
 from question_answering import routes as qa
 from filtering import routes as filtering
 from authentication import routes as auth
-from utils import generate_random_id
+from utils import dummy_answer
 
 app = FastAPI()
 
@@ -14,4 +14,10 @@ app.include_router(summarize.router, prefix="/summarize")
 app.include_router(qa.router, prefix="/question")
 app.include_router(filtering.router, prefix="/filter")
 app.include_router(auth.router, prefix="/auth")
+
+
+@app.post("/chat")
+def chat():
+    return dummy_answer()
+
 
