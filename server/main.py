29d1723c21660f8660cea258aaa109c0ac388c52
LZ <ge94zeg@mytum.de>
2023-11-18 09:47:00 +0100

    added routes to main

--- server/main.py
+++ server/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from summarization import routes as summarize
 from question_answering import routes as qa
-from filtering import routes as filter
+from filtering import routes as filtering
 from authentication import routes as auth
+from utils import generate_random_id
 
 app = FastAPI()
 
@@ -11,5 +12,6 @@ app = FastAPI()
 # include routes to the multiple services
 app.include_router(summarize.router, prefix="/summarize")
 app.include_router(qa.router, prefix="/question")
-app.include_router(filter.router, prefix="/filter")
+app.include_router(filtering.router, prefix="/filter")
 app.include_router(auth.router, prefix="/auth")
+
