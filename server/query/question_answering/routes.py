de66551d633c8fe514c8a430404f6a92f3f1825a
LZ <ge94zeg@mytum.de>
2023-11-18 20:59:35 +0100

    correct parameters

--- server/query/question_answering/routes.py
+++ server/query/question_answering/routes.py
@@ -1,10 +1,23 @@
+import time
+
 from fastapi import APIRouter
+from server.query import log_qa
+from server.query import generate_timestamp
+
 
 # file in charge of summarization of logfiles
 
 router = APIRouter()
 
 
-@router.post("/", tags=["qa"])
-def question_answering(question: str):
-    return {"type": "Q_AND_A", "result": f"your answer to this question '{question}'"}
+@router.post("", tags=["query"])
+def question_answering(query: str, logId: str, top_n_lines: int = 1):
+    top_n_lines = max(top_n_lines, 1)
+    answer, ids = log_qa.generate_llm_answer(query, top_n_lines)
+    logs = [{"lineId": i, "content": log_qa.get_log_line_by_id(i)} for i in ids]
+    result = {
+        "answer": answer,
+        "timestamp": generate_timestamp(),
+        "logs": logs,
+    }
+    return result
