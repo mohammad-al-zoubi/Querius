eb11d5c41ac6e6ed77b950488afb1c8b561409f5
LZ <ge94zeg@mytum.de>
2023-11-18 14:25:53 +0100

    added app.py

---
+++ server/app.py
@@ -0,0 +1,38 @@
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+from query import routes as query
+from authentication import routes as auth
+from utils import dummy_answer
+from logs import routes as logs
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app = FastAPI()
+
+# uvicorn main:app --reload
+
+# include routes to the multiple services
+app.include_router(auth.router, prefix="/auth")
+app.include_router(logs.router, prefix="/log")
+app.include_router(query.router, prefix="/query")
+
+
+@app.post("/query", tags=["query"])
+def query():
+    return dummy_answer()
+
+
+@app.get("/", tags=["root"])
+def root():
+    return {"status": "up and running"}
+
--- server/main.py
+++ server/main.py
@@ -1,26 +1,4 @@
-from fastapi import FastAPI
-from query.summarization import routes as summarize
-from query.question_answering import routes as qa
-from query import routes as query
-from authentication import routes as auth
-from utils import dummy_answer
-from logs import routes as logs
+import uvicorn
 
-app = FastAPI()
-
-# uvicorn main:app --reload
-
-# include routes to the multiple services
-app.include_router(auth.router, prefix="/auth")
-app.include_router(logs.router, prefix="/log")
-app.include_router(query.router, prefix="/query")
-
-
-@app.post("/query")
-def query():
-    return dummy_answer()
-
-
-@app.get("/test")
-def test():
-    return {"status": "up and running"}
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
