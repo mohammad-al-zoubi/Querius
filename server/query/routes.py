3a82bb708c9b91c926a37137d39b2e3af9bf5b56
LZ <ge94zeg@mytum.de>
2023-11-19 00:57:11 +0100

    better error handling

--- server/query/routes.py
+++ server/query/routes.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from starlette import status
+
+from server.helpers.uuid_utils import is_valid_uuid
 from server.query.log_search import routes as search
 from server.query.summarization import routes as summarize
 from server.query.question_answering import routes as qa
@@ -22,6 +25,11 @@ def set_session_parameters(file_path: str):
 
 @router.post("/get_logs_by_line_number", tags=["query"])
 def get_logs_by_line_number(logId: str, line_number: int, neighbor_range: int = 0):
+    if not is_valid_uuid(logId):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="The id provided is not valid",
+        )
     log_qa.set_session_parameters(log_file_db.get(logId))
     line_number -= 1  # Compensate for offset
     neighbor_range = max(neighbor_range, 0)
