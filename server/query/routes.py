7acef4a6194e4f86cc2a2079b62ffda64a45ff7e
LZ <ge94zeg@mytum.de>
2023-11-18 20:59:57 +0100

    new endpoints

--- server/query/routes.py
+++ server/query/routes.py
@@ -1,10 +1,34 @@
+import time
 from fastapi import APIRouter
-from query.filtering import routes as filtering
-from query.summarization import routes as summarize
-from query.question_answering import routes as qa
+from server.query.log_search import routes as search
+from server.query.summarization import routes as summarize
+from server.query.question_answering import routes as qa
+from server.query import log_qa
+
+
 # Main router for querying
 router = APIRouter()
 
-router.include_router(summarize.router, prefix="/summarize")
-router.include_router(qa.router, prefix="/question")
-router.include_router(filtering.router, prefix="/filter")
+router.include_router(summarize.router, prefix="/summary")
+router.include_router(qa.router, prefix="/qa")
+router.include_router(search.router, prefix="/search")
+
+
+@router.post("/set_parameters", tags=["query"])
+def set_session_parameters(file_path: str):
+    log_qa.update_file_tracker()
+    log_qa.set_session_parameters(file_path)
+    return {"result": "parameters set successfully"}
+
+
+@router.post("/get_logs_by_line_number", tags=["query"])
+def get_logs_by_line_number(line_number: int, neighbor_range: int = 0):
+    line_number -= 1  # Compensate for offset
+    neighbor_range = max(neighbor_range, 0)
+    result = []
+    for i in range(line_number - neighbor_range, line_number + neighbor_range + 1):
+        try:
+            result.append(log_qa.get_log_line_by_id(i))
+        except:
+            pass
+    return result
