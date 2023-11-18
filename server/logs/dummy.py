ea98d1e554497dc7852e9c66663b3f6f0c7efca6
LZ <ge94zeg@mytum.de>
2023-11-18 21:53:53 +0100

    new infos added

--- server/logs/dummy.py
+++ server/logs/dummy.py
@@ -4,28 +4,37 @@ log_db = {
             {
                 "logId": "19e303ce-32e3-493d-b710-d031ff162e90",
                 "fileName": f"final_log.out",
-                "description": "final log",
-                "uploadTime": 1700260617547,
-                "isProcessed": False,
-                "size": 21,
+                "description": "final log for the big presentation",
+                "uploadTime": 1700338042,
+                "firstLogTime": 1699591777,
+                "lastLogTime": 1699955418,
+                "isProcessed": True,
+                "lines": 178196,
+                "size": "20.15 MB",
             },
         "78a99ab5-fe4d-4ede-b20b-86fa10c70d58":
             {
                 "logId": "78a99ab5-fe4d-4ede-b20b-86fa10c70d58",
                 "fileName": f"test_log1.out",
                 "description": "log used for testing & development",
-                "uploadTime": 9428960614547,
-                "isProcessed": False,
-                "size": 4,
+                "uploadTime": 1700338042,
+                "firstLogTime": 1699531895,
+                "lastLogTime": 1699591755,
+                "isProcessed": True,
+                "lines": 29295,
+                "size": "3.47 MB",
             },
         "32d86298-c3d1-4524-97f6-a1bc790ae6f8":
             {
                 "logId": "32d86298-c3d1-4524-97f6-a1bc790ae6f8",
                 "fileName": f"test_log2.out",
                 "description": "log used for testing & development",
-                "uploadTime": 75289606763547,
-                "isProcessed": False,
-                "size": 6,
+                "uploadTime": 1700338042,
+                "firstLogTime": 1699445506,
+                "lastLogTime": 1699531873,
+                "isProcessed": True,
+                "lines": 51459,
+                "size": "6.40 MB",
             }
     }
 }
