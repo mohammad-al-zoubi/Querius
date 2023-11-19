ccf02dfe77cbc46501cfe8f09a7d2f5fd974c607
LZ <ge94zeg@mytum.de>
2023-11-19 04:39:44 +0100

    comments

--- server/logs/routes.py
+++ server/logs/routes.py
@@ -9,12 +9,14 @@ router = APIRouter()
 
 @router.get("/all", tags=["logs"])
 def get_all_logs(organization: str = Depends(get_current_organization)):
+    # Return all logs belonging to the organization
     db_helper = DBHelper()
     return db_helper.get_log_list(organization)
 
 
 @router.get("/{log_uuid}", tags=["logs"])
 def get_log(log_uuid: str, organization: str = Depends(get_current_organization)):
+    # Get log by uuid. The log to retrieve has to belong to the given (on JWT token) organization
     if not is_valid_uuid(log_uuid):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
