03a438a364a64f3b6d2642dad0b060c36e8856e4
LZ <ge94zeg@mytum.de>
2023-11-19 08:00:35 +0100

    safety

--- server/query/__init__.py
+++ server/query/__init__.py
@@ -4,8 +4,11 @@ from server.logs.dummy import log_file_db
 
 log_qa_dict = {}
 for i, path in log_file_db.items():
-    lqa = LogQA()
-    lqa.set_session_parameters(path)
-    log_qa_dict[i] = lqa
+    try:
+        lqa = LogQA()
+        lqa.set_session_parameters(path)
+        log_qa_dict[i] = lqa
+    except:
+        log_qa_dict[i] = None
 
 print(log_qa_dict)
