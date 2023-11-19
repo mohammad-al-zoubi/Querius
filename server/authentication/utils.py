f8dfaeab7fcc3f9a5911c71e17011fa5edb870e7
LZ <ge94zeg@mytum.de>
2023-11-19 04:26:32 +0100

    comments

--- server/authentication/utils.py
+++ server/authentication/utils.py
@@ -9,8 +9,10 @@ def create_token(payload):
 
 
 def get_current_user(token):
+    # Extract the current user from a JWT token
     return token_helper.get_current_user(token)
 
 
 def compare_to_hash(password, pw_hash):
+    # Given a password, check its validity by hashing
     return password_helper.compare_to_hash(password, pw_hash)
